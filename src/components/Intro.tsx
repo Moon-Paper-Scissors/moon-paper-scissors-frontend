a9b348059fcb5c187601228cd230fec37bef7b60
Cole Killian <g.cole.killian@gmail.com>
2021-09-21 19:33:52 -0400

    feat: add basic home page

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,14 +1,62 @@
-import { FC } from 'react';
+import { useWallet, WalletStatus } from '@terra-money/wallet-provider';
 
-const Intro: FC = () => (
-  <div className="flex flex-col items-center justify-center space-y-8">
-    <h1 className="text-5xl md:text-7xl text-center dark:text-white">
-      Moon Paper Scissors, on Terra
-    </h1>
-    <p className="max-w-xs md:max-w-prose text-2xl md:text-3xl text-center dark:text-white">
-      May the best lunatic win :)
-    </p>
-  </div>
-);
+const Intro = () => {
+  const {
+    status,
+    network,
+    wallets,
+    availableConnectTypes,
+    availableInstallTypes,
+    connect,
+    install,
+    disconnect,
+  } = useWallet();
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-8">
+      <h1 className="text-5xl md:text-7xl text-center dark:text-white">
+        Moon Paper Scissors, on Terra
+      </h1>
+      <p className="max-w-xs md:max-w-prose text-2xl md:text-3xl text-center dark:text-white">
+        May the best lunatic win :)
+      </p>
+      <div className="flex flex-row items-center justify-center">
+        {status === WalletStatus.WALLET_NOT_CONNECTED && (
+          <>
+            {availableInstallTypes.map((connectType) => (
+              <button
+                type="button"
+                className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400"
+                key={`install-${connectType}`}
+                onClick={() => install(connectType)}
+              >
+                Install {connectType}
+              </button>
+            ))}
+            {availableConnectTypes.map((connectType) => (
+              <button
+                type="button"
+                className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400"
+                key={`connect-${connectType}`}
+                onClick={() => connect(connectType)}
+              >
+                Connect {connectType}
+              </button>
+            ))}
+          </>
+        )}
+        {status === WalletStatus.WALLET_CONNECTED && (
+          <button
+            type="button"
+            className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400"
+            onClick={() => disconnect()}
+          >
+            Disconnect
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,31 @@
 import '@/styles/tailwind.css';
+import {
+  NetworkInfo,
+  StaticWalletProvider,
+  WalletProvider,
+} from '@terra-money/wallet-provider';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { FC } from 'react';
 import 'tailwindcss/tailwind.css';
+
+const mainnet = {
+  name: `mainnet`,
+  chainID: `columbus-4`,
+  lcd: `https://lcd.terra.dev`,
+};
+
+const testnet = {
+  name: `testnet`,
+  chainID: `tequila-0004`,
+  lcd: `https://tequila-lcd.terra.dev`,
+};
+
+const walletConnectChainIds: Record<number, NetworkInfo> = {
+  0: testnet,
+  1: mainnet,
+};
 
 const meta = {
   title: `Moon Paper Scissors, on Terra`,
@@ -12,46 +34,59 @@ const meta = {
   image: ``,
 };
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>{meta.title}</title>
-      <meta name="title" content={meta.title} />
-      <meta name="description" content={meta.description} />
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const main = (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="title" content={meta.title} />
+        <meta name="description" content={meta.description} />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={meta.url} />
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:description" content={meta.description} />
-      <meta property="og:image" content={meta.url + meta.image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={meta.url} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.url + meta.image} />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={meta.url} />
-      <meta property="twitter:title" content={meta.title} />
-      <meta property="twitter:description" content={meta.description} />
-      <meta property="twitter:image" content={meta.url + meta.image} />
-    </Head>
-    <ThemeProvider enableSystem enableColorScheme attribute="class">
-      <div className="flex flex-col items-center min-h-screen dark:bg-black">
-        <div className="flex flex-1 items-center justify-center mt-4">
-          <Component {...pageProps} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={meta.url} />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={meta.url + meta.image} />
+      </Head>
+      <ThemeProvider enableSystem enableColorScheme attribute="class">
+        <div className="flex flex-col items-center min-h-screen dark:bg-black">
+          <div className="flex flex-1 items-center justify-center mt-4">
+            <Component {...pageProps} />
+          </div>
+          <div className="my-4">
+            <p className="text-xl dark:text-white">
+              Created by{` `}
+              <a
+                className="underline"
+                href="https://twitter.com/colekillian_"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Cole Killian
+              </a>
+            </p>
+          </div>
         </div>
-        <div className="my-4">
-          <p className="text-xl dark:text-white">
-            Created by{` `}
-            <a
-              className="underline"
-              href="https://twitter.com/colekillian_"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Cole Killian
-            </a>
-          </p>
-        </div>
-      </div>
-    </ThemeProvider>
-  </>
-);
+      </ThemeProvider>
+    </>
+  );
+
+  return typeof window !== `undefined` ? (
+    <WalletProvider
+      defaultNetwork={mainnet}
+      walletConnectChainIds={walletConnectChainIds}
+    >
+      {main}
+    </WalletProvider>
+  ) : (
+    <StaticWalletProvider defaultNetwork={mainnet}>{main}</StaticWalletProvider>
+  );
+};
 
 export default App;
