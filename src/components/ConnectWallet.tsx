66126ece4e3000fa2a47f3db9d06e92e1405c510
Cole Killian <g.cole.killian@gmail.com>
2021-10-05 11:50:18 -0400

    feat: clarify that you need chrome extension

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -54,25 +54,35 @@ const ConnectWallet = ({
             />
           </button>
         </div>
-        {availableConnectTypes
-          .filter(
-            (connectType) => [`CHROME_EXTENSION`].includes(connectType),
-            // , `WALLETCONNECT`
-          )
-          .map((connectType) => (
-            <button
-              type="button"
-              className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400 mt-10"
-              key={`connect-${connectType}`}
-              style={{ maxWidth: `300px` }}
-              onClick={() => connect(connectType)}
-            >
-              Connect{` `}
-              {connectType === `CHROME_EXTENSION`
-                ? `Terra Station Extension`
-                : `Terra Station Mobile`}
-            </button>
-          ))}
+
+        <>
+          {(() => {
+            const chromeConnectType = availableConnectTypes.find(
+              (connectType) => connectType === `CHROME_EXTENSION`,
+            );
+            if (chromeConnectType) {
+              return (
+                <button
+                  type="button"
+                  className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400 mt-10"
+                  key={`connect-${chromeConnectType}`}
+                  style={{ maxWidth: `300px` }}
+                  onClick={() => connect(chromeConnectType)}
+                >
+                  Connect{` `}
+                  {chromeConnectType === `CHROME_EXTENSION`
+                    ? `Terra Station Extension`
+                    : `Terra Station Mobile`}
+                </button>
+              );
+            }
+            return (
+              <p className="p-4 border-4 border-current max-w-md text-3xl text-center dark:text-white mt-10">
+                Please Install the Terra Station Extension to Continue
+              </p>
+            );
+          })()}
+        </>
       </div>
     </div>
   );
@@ -90,40 +100,60 @@ export default ConnectWallet;
           </button>
         )} */
 
-//     <div className="flex flex-col min-h-screen items-center justify-center space-y-8">
-//       <div
-//         className="flex items-center justify-evenly"
-//         style={{ width: `700px` }}
-//       >
-//         {status === WalletStatus.WALLET_NOT_CONNECTED && (
-//           <>
-//             {(() => {
-//               const chromeConnectType = availableConnectTypes.find(
-//                 (connectType) => connectType === `CHROME_EXTENSION`,
-//               );
-//               if (chromeConnectType) {
-//                 return (
-//                   <button
-//                     type="button"
-//                     className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400"
-//                     key={`connect-${chromeConnectType}`}
-//                     style={{ maxWidth: `300px` }}
-//                     onClick={() => connect(chromeConnectType)}
-//                   >
-//                     Connect{` `}
-//                     {chromeConnectType === `CHROME_EXTENSION`
-//                       ? `Terra Station Extension`
-//                       : `Terra Station Mobile`}
-//                   </button>
-//                 );
-//               }
-//               return (
-//                 <p className="p-4 border-4 border-current max-w-md text-3xl text-center dark:text-white">
-//                   Please Install the Terra Station Extension to Continue
-//                 </p>
-//               );
-//             })()}
-//           </>
-//         )}
-//       </div>
-//     </div>
+// <div className="flex flex-col min-h-screen items-center justify-center space-y-8">
+//   <div
+//     className="flex items-center justify-evenly"
+//     style={{ width: `700px` }}
+//   >
+//     {status === WalletStatus.WALLET_NOT_CONNECTED && (
+//       <>
+//         {(() => {
+//           const chromeConnectType = availableConnectTypes.find(
+//             (connectType) => connectType === `CHROME_EXTENSION`,
+//           );
+//           if (chromeConnectType) {
+//             return (
+//               <button
+//                 type="button"
+//                 className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400"
+//                 key={`connect-${chromeConnectType}`}
+//                 style={{ maxWidth: `300px` }}
+//                 onClick={() => connect(chromeConnectType)}
+//               >
+//                 Connect{` `}
+//                 {chromeConnectType === `CHROME_EXTENSION`
+//                   ? `Terra Station Extension`
+//                   : `Terra Station Mobile`}
+//               </button>
+//             );
+//           }
+//           return (
+//             <p className="p-4 border-4 border-current max-w-md text-3xl text-center dark:text-white">
+//               Please Install the Terra Station Extension to Continue
+//             </p>
+//           );
+//         })()}
+//       </>
+//     )}
+//   </div>
+// </div>
+
+// {availableConnectTypes
+//   .filter(
+//     (connectType) => [`CHROME_EXTENSION`].includes(connectType),
+//     // , `WALLETCONNECT`
+//   )
+//   .map((connectType) => (
+//     <button
+//       type="button"
+//       className="text-3xl p-4 border-4 border-current text-black dark:text-white hover:text-gray-500 dark:hover:text-gray-400 mt-10"
+//       key={`connect-${connectType}`}
+//       style={{ maxWidth: `300px` }}
+//       onClick={() => connect(connectType)}
+//     >
+//       Connect{` `}
+//       {connectType === `CHROME_EXTENSION`
+//         ? `Terra Station Extension`
+//         : `Terra Station Mobile`}
+//     </button>
+//   ))}
